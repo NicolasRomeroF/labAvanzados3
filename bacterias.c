b4a56d6ea1a02e73c86c6c9d8a29a5aaf643d5df
Nico <nicolas.romero.f@usach.cl>
2018-07-01 20:00:47 -0400

    ready for testing

--- bacterias.c
+++ bacterias.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <limits.h>
 
 int sizeCadena(FILE* archivo)
 {
@@ -27,8 +29,120 @@ char* leerArchivo(FILE* archivo)
     char* distribucion = (char*)malloc(sizeof(char)*size);
 
     fscanf(archivo,"%s",distribucion);
-    printf("size:%d %s",size,distribucion);
     return distribucion;
+}
+
+void unir(char* distribucion, int* min, char valor, char* auxDist, int auxSize, int i)
+{
+    char* end;
+    auxDist=(char*)malloc(sizeof(char)*(auxSize)-1);
+    int contU = 0;
+    
+    while(contU<i)
+    {
+        auxDist[contU]=distribucion[contU];
+        contU++;
+    }
+    auxDist[i]=valor;
+    contU=i+2;
+    while(contU<auxSize)
+    {
+        auxDist[contU-1]=distribucion[contU];
+        contU++;
+    }
+    int aux = strtol(auxDist,&end,10);
+    int test = *min;
+    if(test>aux)
+    {
+        *min=aux;
+    }
+}
+
+void unirse(FILE* salida, char* distribucion)
+{
+    fprintf(salida,"%s\n",distribucion);
+    int i;
+    int auxSize = strlen(distribucion)+1;
+    char* end;
+    int min = strtol(distribucion,&end,10);
+    char* auxDist=NULL;
+    int flag = 1;
+    int cambio = 0;
+    while(flag == 1 )
+    {
+        i = 0;
+        while(i+1<auxSize)
+        {
+            if(distribucion[i] == '1')
+            {
+                if(distribucion[i+1] == '2')
+                { 
+                    unir(distribucion, &min, '3', auxDist, auxSize, i);
+                    free(auxDist);
+                    cambio = 1;
+                }
+                if(distribucion[i+1] == '3')
+                {
+                    unir(distribucion, &min, '2', auxDist, auxSize, i);
+                    free(auxDist);
+                    cambio = 1;
+                }
+            }
+            if(distribucion[i] == '2')
+            {
+                if(distribucion[i+1] == '1')
+                {
+                    unir(distribucion, &min, '3', auxDist, auxSize, i);
+                    free(auxDist);
+                    cambio = 1;
+                }
+                if(distribucion[i+1] == '3')
+                {
+                    unir(distribucion, &min, '1', auxDist, auxSize, i);
+                    free(auxDist);
+                    cambio = 1;
+                }
+            }
+            if(distribucion[i] == '3')
+            {
+                if(distribucion[i+1] == '1')
+                {
+                    unir(distribucion, &min, '2', auxDist, auxSize, i);
+                    free(auxDist);
+                    cambio = 1;
+                }
+                if(distribucion[i+1] == '2')
+                {
+                    unir(distribucion, &min, '1', auxDist, auxSize, i);
+                    free(auxDist);
+                    cambio = 1;
+                }
+            }          
+            i++;
+        }
+        if(cambio==1)
+        {        
+            if(auxSize>0)
+            {
+                cambio = 0;
+            }
+            else
+            {
+                flag = 0;
+            }
+            fprintf(salida,"%d\n",min);
+        }
+        else
+        {
+            flag = 0;
+        }
+        
+
+        auxSize--;
+        free(distribucion);
+        distribucion=(char*)malloc(sizeof(char)*auxSize);
+        sprintf(distribucion,"%d",min);
+    }
 }
 
 int main(){
@@ -38,7 +152,17 @@ int main(){
         printf("Error al abrir el archivo de entrada \n");
         return 1;
     }
-    leerArchivo(entrada);
+    char* distribucion = leerArchivo(entrada);
+    fclose(entrada);
+
+    FILE* salida = fopen("Salida.out","w");
+    if(salida==NULL)
+    {
+        printf("Error al crear archivo de salida\n");
+        return 1;
+    }
+    unirse(salida,distribucion);
+    fclose(salida);
     
     return 0;
 }
